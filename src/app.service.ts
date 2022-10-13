f960d1158d2580aba3a0522bb15546eaa7839ed0
Jesus Mejia <chucho4720013@gmail.com>
2022-10-12 23:56:29 -0500

    Connection as provider

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,30 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg';
+
 import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
     // @Inject('API_KEY') private apiKey: string,
+    @Inject('PG') private clientPG: Client,
     @Inject('TASKS') private tasks: any[],
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
   getHello(): string {
     const apiKey = this.configService.apiKey;
     return apiKey;
+  }
+
+  getNowPG() {
+    return new Promise((resolve, reject) => {
+      this.clientPG.query('SELECT NOW()', (err, res) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(res.rows[0]);
+      });
+    });
   }
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,18 @@
 import { Global, Module } from '@nestjs/common';
+import { Client } from 'pg';
 
 const API_KEY = '123456';
 const API_KEY_PROD = 'PROD123456';
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'admin',
+  password: '1234',
+  database: 'postgres',
+});
+
+client.connect();
 
 @Global()
 @Module({
@@ -10,7 +21,11 @@ const API_KEY_PROD = 'PROD123456';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'PG',
+      useValue: client,
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'PG'],
 })
 export class DatabaseModule {}
