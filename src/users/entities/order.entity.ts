14af83fbb680ba2ce7ba08dc60cf0df7a0a2bcb6
Jesus Mejia <chucho4720013@gmail.com>
2022-10-18 00:42:25 -0500

    Serialize

--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude, Expose } from 'class-transformer';
 import {
   CreateDateColumn,
   Entity,
@@ -31,6 +32,28 @@ export class Order {
     name: 'order_id',
   })
   customer: Customer;
+  @Exclude()
   @OneToMany(() => OrderItem, (orderItem) => orderItem.order)
   items: OrderItem[];
+  @Expose()
+  get products() {
+    if (this.items) {
+      return this.items
+        .filter((item) => !!item)
+        .map((item) => ({
+          ...item.product,
+          quantity: item.quantity,
+        }));
+    }
+    return [];
+  }
+  @Expose()
+  get total() {
+    if (this.items) {
+      return this.items
+        .filter((item) => !!item)
+        .reduce((total, item) => total + item.quantity * item.product.price, 0);
+    }
+    return 0;
+  }
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer';
 import {
   Column,
   CreateDateColumn,
@@ -17,6 +18,7 @@ export class User {
   @Column({ type: 'varchar', length: 255, unique: true })
   email: string;
   @Column({ type: 'varchar', length: 255 })
+  @Exclude()
   password: string;
   @Column({ type: 'varchar', length: 100 })
   role: string;
