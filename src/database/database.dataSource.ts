a403cecb6aa40460a0da85ab57a29d5f30bd8117
Jesus Mejia <chucho4720013@gmail.com>
2022-10-23 18:54:05 -0500

    Migrations in production

--- src/database/database.dataSource.ts
+++ src/database/database.dataSource.ts
@@ -5,11 +5,7 @@ config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT, 10),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  url: process.env.DATABASE_URL,
   ssl:
     process.env.NODE_ENV === 'production'
       ? { rejectUnauthorized: false }
